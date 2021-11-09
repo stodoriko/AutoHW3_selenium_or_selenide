b3b36480550a008548b46c8814fb6207544d0a2f
stodoriko <s.todorikos@gmail.com>
2021-11-09 20:32:58 +0300

    added revision

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -12,7 +12,6 @@ class SeleniumTest {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
         WebDriverManager.chromedriver().setup();
     }
 
@@ -23,6 +22,7 @@ class SeleniumTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999/");
     }
 
     @AfterEach
@@ -33,43 +33,71 @@ class SeleniumTest {
 
     @Test
     void shouldSetPersonalInfoTest() {
-        driver.get("http://localhost:9999/");
-        driver.findElement(By.xpath(".//span[preceding-sibling::span[contains(text(), 'Фамилия и имя')]]" +
-                "//input[contains(@class, 'input__control')]")).sendKeys("Тодорико Сергей");
-        driver.findElement(By.xpath(".//span[preceding-sibling::span[contains(text(), 'Мобильный телефон')]]" +
-                "//input[contains(@class, 'input__control')]")).sendKeys("+79258582575");
+        driver.findElement(By.xpath("//span[@data-test-id='name']//input")).sendKeys("Тодорико Сергей");
+        driver.findElement(By.xpath("//span[@data-test-id='phone']//input")).sendKeys("+79258582575");
         driver.findElement(By.xpath(".//span[contains(@class, 'checkbox__box')]")).click();
         driver.findElement(By.xpath(".//span[contains(text(), 'Продолжить')]")).click();
-        String actualMessage = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
+        String actualMessage = driver.findElement(By.xpath("//div[contains(@class, 'Success_successBlock')]//p")).getText();
         String expectedMessage = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         Assertions.assertEquals(expectedMessage, actualMessage.strip());
     }
 
     @Test
     void shouldFailIfNameIsNotRussian() {
-        driver.get("http://localhost:9999/");
-        driver.findElement(By.xpath(".//span[preceding-sibling::span[contains(text(), 'Фамилия и имя')]]" +
-                "//input[contains(@class, 'input__control')]")).sendKeys("Todoriko Sergei");
-        driver.findElement(By.xpath(".//span[preceding-sibling::span[contains(text(), 'Мобильный телефон')]]" +
-                "//input[contains(@class, 'input__control')]")).sendKeys("+79258582575");
+        driver.findElement(By.xpath("//span[@data-test-id='name']//input")).sendKeys("Todoriko Sergei");
+        driver.findElement(By.xpath("//span[@data-test-id='phone']//input")).sendKeys("+79258582575");
         driver.findElement(By.xpath(".//span[contains(@class, 'checkbox__box')]")).click();
         driver.findElement(By.xpath(".//span[contains(text(), 'Продолжить')]")).click();
-        String actualMessage = driver.findElement(By.xpath(".//span[preceding-sibling::span//input[contains(@name, 'name')]]")).getText();
+        String actualMessage = driver.findElement(By.xpath("//span[contains(@class, 'input_invalid')]" +
+                "//span[preceding-sibling::span//input[contains(@name, 'name')]][contains(@class, 'input__sub')]")).getText();
         String expectedMessage = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
         Assertions.assertEquals(expectedMessage, actualMessage);
     }
 
     @Test
     void shouldFailIfWrongNumber() {
-        driver.get("http://localhost:9999/");
-        driver.findElement(By.xpath(".//span[preceding-sibling::span[contains(text(), 'Фамилия и имя')]]" +
-                "//input[contains(@class, 'input__control')]")).sendKeys("Тодорико Сергей");
-        driver.findElement(By.xpath(".//span[preceding-sibling::span[contains(text(), 'Мобильный телефон')]]" +
-                "//input[contains(@class, 'input__control')]")).sendKeys("+7925858257");
+        driver.findElement(By.xpath("//span[@data-test-id='name']//input")).sendKeys("Тодорико Сергей");
+        driver.findElement(By.xpath("//span[@data-test-id='phone']//input")).sendKeys("+7925858257");
         driver.findElement(By.xpath(".//span[contains(@class, 'checkbox__box')]")).click();
         driver.findElement(By.xpath(".//span[contains(text(), 'Продолжить')]")).click();
-        String actualMessage = driver.findElement(By.xpath(".//span[preceding-sibling::span//input[contains(@name, 'phone')]]")).getText();
+        String actualMessage = driver.findElement(By.xpath("//span[contains(@class, 'input_invalid')]" +
+                "//span[preceding-sibling::span//input[contains(@name, 'phone')]][contains(@class, 'input__sub')]")).getText();
         String expectedMessage = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        Assertions.assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    void shouldFailIfNameInputIsEmpty() {
+        driver.findElement(By.xpath("//span[@data-test-id='name']//input")).sendKeys("");
+        driver.findElement(By.xpath("//span[@data-test-id='phone']//input")).sendKeys("+79258582575");
+        driver.findElement(By.xpath(".//span[contains(@class, 'checkbox__box')]")).click();
+        driver.findElement(By.xpath(".//span[contains(text(), 'Продолжить')]")).click();
+        String actualMessage = driver.findElement(By.xpath("//span[contains(@class, 'input_invalid')]" +
+                "//span[preceding-sibling::span//input[contains(@name, 'name')]][contains(@class, 'input__sub')]")).getText();
+        String expectedMessage = "Поле обязательно для заполнения";
+        Assertions.assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    void shouldFailIfPhoneInputIsEmpty() {
+        driver.findElement(By.xpath("//span[@data-test-id='name']//input")).sendKeys("Тодорико Сергей");
+        driver.findElement(By.xpath("//span[@data-test-id='phone']//input")).sendKeys("");
+        driver.findElement(By.xpath(".//span[contains(@class, 'checkbox__box')]")).click();
+        driver.findElement(By.xpath(".//span[contains(text(), 'Продолжить')]")).click();
+        String actualMessage = driver.findElement(By.xpath("//span[contains(@class, 'input_invalid')]" +
+                "//span[preceding-sibling::span//input[contains(@name, 'phone')]][contains(@class, 'input__sub')]")).getText();
+        String expectedMessage = "Поле обязательно для заполнения";
+        Assertions.assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    void shouldFailIfCheckboxIsEmpty() {
+        driver.findElement(By.xpath("//span[@data-test-id='name']//input")).sendKeys("Тодорико Сергей");
+        driver.findElement(By.xpath("//span[@data-test-id='phone']//input")).sendKeys("+79258582575");
+        driver.findElement(By.xpath(".//span[contains(text(), 'Продолжить')]")).click();
+        String actualMessage = driver.findElement(By.xpath("//label[contains(@class, 'input_invalid')]//span[contains(@class, 'checkbox__text')]")).getText();
+        System.out.println(actualMessage);
+        String expectedMessage = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
         Assertions.assertEquals(expectedMessage, actualMessage);
     }
 }
